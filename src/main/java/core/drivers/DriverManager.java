9817d96b74a623110ec5860324ca1e6e4637de30
Noushin <nou.babakhani@gmail.com>
2024-07-25 17:02:35 +0100

    Added DriverManager class

---
+++ src/main/java/core/drivers/DriverManager.java
@@ -0,0 +1,34 @@
+package core.drivers;
+
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverManager {
+    // ThreadLocal to store WebDriver instances for each thread
+    private static final ThreadLocal<WebDriver> driverThread = new ThreadLocal<>();
+
+    /*
+    Gets the WebDriver instance for the current thread.
+     */
+    public static WebDriver getDriver() {
+        return driverThread.get();
+    }
+
+    /*
+    Sets the WebDriver instance for the current thread.
+     */
+    public static void setDriver(WebDriver webDriver) {
+        driverThread.set(webDriver);
+    }
+
+    /*
+     Removes the WebDriver instance for the current thread and quits the browser.
+     */
+    public static void removeDriver() {
+        WebDriver driver = getDriver();
+        if (driver != null) {
+            driver.quit(); // Close the browser
+            driverThread.remove(); // Remove the WebDriver instance from the ThreadLocal
+        }
+    }
+}
