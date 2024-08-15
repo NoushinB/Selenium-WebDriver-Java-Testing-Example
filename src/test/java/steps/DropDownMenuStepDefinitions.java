2c46a68550191ebc941dd10fafd44ca821fca148
noushinB <nou.babakhani@gmail.com>
2024-08-15 16:57:14 +0100

    DropDownMenuStepDefinitions  updated

--- src/test/java/steps/DropDownMenuStepDefinitions.java
+++ src/test/java/steps/DropDownMenuStepDefinitions.java
@@ -1,15 +1,30 @@
 package steps;
 
+import core.pages.DropDownMenuPage;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.junit.Assert.assertEquals;
+
 public class DropDownMenuStepDefinitions {
+    final private DropDownMenuPage dropDownMenuPage;
+
+    public DropDownMenuStepDefinitions() {
+        dropDownMenuPage = new DropDownMenuPage();
+    }
+
+    @Given("the user is on the dropDownMenu page")
+    public void userIsOnTheDropDownMenuPage() {
+        String dropDownMenu="https://bonigarcia.dev/selenium-webdriver-java/dropdown-menu.html";
+        String currentUrl=dropDownMenuPage.getCurrentPageURL();
+        assertEquals(dropDownMenu,currentUrl);
+    }
 
     @When("User clicks on the {string} dropdown button")
     public void user_clicks_on_dropdown_button(String buttonName) {
-        // Implement code to click the specified dropdown button
-        // Example: driver.findElement(By.id(buttonName)).click();
+    dropDownMenuPage.clickOnButton(buttonName);
     }
 
     @Then("the dropdown menu items should be visible")
