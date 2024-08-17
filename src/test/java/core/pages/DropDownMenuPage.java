7267c768cddab5ac19fd0f5890cfee616e855256
noushinB <nou.babakhani@gmail.com>
2024-08-17 13:11:40 +0100

    Added menuItemClick to DropDownMenuPage and updated it.

--- src/test/java/core/pages/DropDownMenuPage.java
+++ src/test/java/core/pages/DropDownMenuPage.java
@@ -1,6 +1,7 @@
 package core.pages;
 
 import core.pages.common.BasePage;
+import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
@@ -34,8 +35,22 @@ public class DropDownMenuPage extends BasePage {
             case "my-dropdown-1" -> dropdownButton.click();
             case "my-dropdown-2" -> actions.contextClick(dropdownButton).perform();
             case "my-dropdown-3" -> actions.doubleClick(dropdownButton).perform();
-
         }
+    }
+
+    public void menuItemClick(int menuNumber, String itemText) {
+        WebElement item = switch (menuNumber) {
+            case 1 -> findMenuItem(visibleDropDownMenus1, itemText);
+            case 2 -> findMenuItem(visibleDropDownMenus2, itemText);
+            case 3 -> findMenuItem(visibleDropDownMenus3, itemText);
+            default -> throw new IllegalStateException("Unexpected value: " + menuNumber);
+        };
+        Objects.requireNonNull(item, "Menu item not found: " + itemText);
+        item.click();
+    }
+
+    private WebElement findMenuItem(List<WebElement> elements, String itemText) {
+        return elements.stream().filter(item -> Objects.equals(item.getText(), itemText)).findAny().orElse(null);
     }
 
     private WebElement findDropDownButton(String buttonName) {
@@ -48,13 +63,14 @@ public class DropDownMenuPage extends BasePage {
     }
 
     public boolean areMenuItemsVisible(String buttonName) {
-        if (buttonName == "my-dropdown-1") {
-            return visibleDropDownMenus1.size() > 0;
-        } else  if (buttonName == "my-dropdown-2") {
-            return visibleDropDownMenus2.size()>0;
+        if (Objects.equals(buttonName, "my-dropdown-1")) {
+            return !visibleDropDownMenus1.isEmpty();
+        } else if (Objects.equals(buttonName, "my-dropdown-2")) {
+            return !visibleDropDownMenus2.isEmpty();
+        } else if (Objects.equals(buttonName, "my-dropdown-3")) {
+            return !visibleDropDownMenus3.isEmpty();
         } else {
-            return visibleDropDownMenus3.size()>0;
-
+            throw new NotFoundException("Menu not found");
         }
     }
 }
