9ca468ed55395ad1829bd792c371e91c0b3606e4
noushinB <nou.babakhani@gmail.com>
2024-08-22 17:27:26 +0100

    Added elements and methods to DragAndDropPage

--- src/test/java/core/pages/DragAndDropPage.java
+++ src/test/java/core/pages/DragAndDropPage.java
@@ -1,6 +1,29 @@
 package core.pages;
 
 import core.pages.common.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 
 public class DragAndDropPage extends BasePage {
+    private final Actions actions;
+    @FindBy(id = ("draggable"))
+    private WebElement draggableElement;
+    @FindBy(id = ("target"))
+    private WebElement targetElement;
+
+    public DragAndDropPage() {
+        actions = new Actions(driver);
+    }
+
+
+
+    public void dragAndDropElement(){
+        actions.dragAndDrop(draggableElement,targetElement).build().perform();
+    }
+
+    public boolean isElementAtTarget(){
+        return draggableElement.getLocation().equals(targetElement.getLocation());
+    }
+
 }
