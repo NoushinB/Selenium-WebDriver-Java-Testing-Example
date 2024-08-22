69d74c4d2527d9e88d0411770bf9d8d91a49e8aa
noushinB <nou.babakhani@gmail.com>
2024-08-22 17:29:30 +0100

    DragAndDropStepDefinitions updated

--- src/test/java/steps/DragAndDropStepDefinitions.java
+++ src/test/java/steps/DragAndDropStepDefinitions.java
@@ -10,23 +10,24 @@ import static org.junit.Assert.assertEquals;
 public class DragAndDropStepDefinitions {
     final private DragAndDropPage dragAndDropPage;
 
-public DragAndDropStepDefinitions(){
-    dragAndDropPage=new DragAndDropPage();
-}
+    public DragAndDropStepDefinitions() {
+        dragAndDropPage = new DragAndDropPage();
+    }
+
     @Given("the user is on the DragAndDrop page")
     public void userIsOnTheDragAndDropPage() {
         String dropDownMenu = "https://bonigarcia.dev/selenium-webdriver-java/drag-and-drop.html";
         String currentUrl = dragAndDropPage.getCurrentPageURL();
         assertEquals(dropDownMenu, currentUrl);
     }
+
     @When("User drags the draggable element to the target element")
     public void user_drags_the_draggable_element_to_the_target_element() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        dragAndDropPage.dragAndDropElement();
     }
+
     @Then("Draggable element should be at the same location as the target element")
     public void draggable_element_should_be_at_the_same_location_as_the_target_element() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        dragAndDropPage.isElementAtTarget();
     }
 }
