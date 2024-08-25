afb4bb93b171314e5a042499ba1ac0259989e118
noushinB <nou.babakhani@gmail.com>
2024-08-25 15:55:13 +0100

    updated webFormComponents.feature

---
+++ src/test/java/steps/WebFormComponents.java
@@ -0,0 +1,232 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class WebFormComponents {
+    @Given("User is on the {string} page")
+    public void user_is_on_the_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters {string} into the text input field")
+    public void user_enters_into_the_text_input_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the text input field should display {string}")
+    public void the_text_input_field_should_display(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters {string} into the password field")
+    public void user_enters_into_the_password_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the password field should display an obscured format of the entered password")
+    public void the_password_field_should_display_an_obscured_format_of_the_entered_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters {string} into the textarea")
+    public void user_enters_into_the_textarea(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the textarea should display {string}")
+    public void the_textarea_should_display(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the input field is disabled")
+    public void the_input_field_is_disabled() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User tries to enter text into the disabled input field")
+    public void user_tries_to_enter_text_into_the_disabled_input_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the input field should not accept any text")
+    public void the_input_field_should_not_accept_any_text() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User attempts to change the value to {string}")
+    public void user_attempts_to_change_the_value_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the input field is readonly with the value {string}")
+    public void the_input_field_is_readonly_with_the_value(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @Then("the input field should still display {string}")
+    public void the_input_field_should_still_display(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User should not be able to modify the value")
+    public void user_should_not_be_able_to_modify_the_value() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User should be redirected to the {string} page")
+    public void user_should_be_redirected_to_the_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User opens the dropdown menu")
+    public void user_opens_the_dropdown_menu() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User selects {string} from the dropdown")
+    public void user_selects_from_the_dropdown(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the dropdown should display {string}")
+    public void the_dropdown_should_display(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User types {string} into the datalist input field")
+    public void user_types_into_the_datalist_input_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the datalist should display matching suggestions")
+    public void the_datalist_should_display_matching_suggestions() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User selects {string} from the datalist suggestions")
+    public void user_selects_from_the_datalist_suggestions(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the input field should display {string}")
+    public void the_input_field_should_display(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User selects a file {string} using the file input")
+    public void user_selects_a_file_using_the_file_input(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the file input should display {string}")
+    public void the_file_input_should_display(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User checks the checkbox")
+    public void user_checks_the_checkbox() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the checkbox should be checked")
+    public void the_checkbox_should_be_checked() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User unchecks the checkbox")
+    public void user_unchecks_the_checkbox() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the checkbox should be unchecked")
+    public void the_checkbox_should_be_unchecked() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User selects the {string} radio button")
+    public void user_selects_the_radio_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the {string} radio button should be checked")
+    public void the_radio_button_should_be_checked(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("other radio buttons should not be selected")
+    public void other_radio_buttons_should_not_be_selected() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User chooses the color {string} from the color picker")
+    public void user_chooses_the_color_from_the_color_picker(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the color picker should display the color {string}")
+    public void the_color_picker_should_display_the_color(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User selects the date {string} from the date picker")
+    public void user_selects_the_date_from_the_date_picker(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the date picker should display {string}")
+    public void the_date_picker_should_display(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User moves the range slider to a value of {string}")
+    public void user_moves_the_range_slider_to_a_value_of(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the range input should display a value of {string}")
+    public void the_range_input_should_display_a_value_of(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
