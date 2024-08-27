01fc2dbd995c3ebdeb6abe134f947fe6c605e4d8
noushinB <nou.babakhani@gmail.com>
2024-08-27 18:08:48 +0100

    Added elements to WebFormComponentsPage

--- src/test/java/core/pages/WebFormComponentsPage.java
+++ src/test/java/core/pages/WebFormComponentsPage.java
@@ -1,6 +1,13 @@
 package core.pages;
 
 import core.pages.common.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class WebFormComponentsPage extends BasePage {
+
+    @FindBy(id = "my-text-id")
+    private WebElement textInput;
+    @FindBy(xpath = "//input[@name='my-password']")
+    private WebElement password;
 }
