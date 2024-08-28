10330eb7f1fdadc1e9845ce37dc7a2bf543a0d5f
noushinB <nou.babakhani@gmail.com>
2024-08-28 16:21:57 +0100

    Added all elements to WebFormComponentsPage

--- src/test/java/core/pages/WebFormComponentsPage.java
+++ src/test/java/core/pages/WebFormComponentsPage.java
@@ -10,4 +10,34 @@ public class WebFormComponentsPage extends BasePage {
     private WebElement textInput;
     @FindBy(xpath = "//input[@name='my-password']")
     private WebElement password;
+    @FindBy(xpath = "//textarea[@name='my-textarea']")
+    private WebElement textArea;
+    @FindBy(xpath = "//input[@placeholder='Disabled input']")
+    private WebElement disabledInput;
+    @FindBy(xpath = "//input[@name='my-readonly']")
+    private WebElement readonlyInput;
+    @FindBy(xpath = "//a[normalize-space()='Return to index']")
+    private WebElement returnToIndex;
+    @FindBy(xpath = "//select[@name='my-select']")
+    private WebElement dropDownSelect;
+    @FindBy(xpath = "//input[@placeholder='Type to search...']")
+    private WebElement dropDownDataList;
+    @FindBy(xpath = "//input[@name='my-file']")
+    private WebElement inputFile;
+    @FindBy(id = "my-check-1")
+    private WebElement checkedCheckbox;
+    @FindBy(id = "my-check-2")
+    private WebElement defaultCheckbox;
+    @FindBy(id = "my-radio-1")
+    private WebElement checkedRadio;
+    @FindBy(id = "my-radio-2")
+    private WebElement defaultRadio;
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement submitButton;
+    @FindBy(xpath = "//input[@name='my-colors']")
+    private WebElement colorPicker;
+    @FindBy(xpath = "//input[@name='my-date']")
+    private WebElement datePicker;
+    @FindBy(xpath = "//input[@name='my-range']")
+    private WebElement rangeSlider;
 }
