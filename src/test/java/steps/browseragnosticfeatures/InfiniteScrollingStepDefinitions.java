af1598249c0a994d9f7a3f6445d733f161915804
noushinB <nou.babakhani@gmail.com>
2024-12-11 15:06:07 +0000

    Added InfiniteScrollingStepDefinitions

---
+++ src/test/java/steps/browseragnosticfeatures/InfiniteScrollingStepDefinitions.java
@@ -0,0 +1,11 @@
+package steps.browseragnosticfeatures;
+
+import core.pages.browseragnosticfeatures.InfiniteScrollingPage;
+
+public class InfiniteScrollingStepDefinitions {
+    private final InfiniteScrollingPage infiniteScrollingPage;
+
+    public InfiniteScrollingStepDefinitions() {
+        infiniteScrollingPage = new  InfiniteScrollingPage();
+    }
+}
