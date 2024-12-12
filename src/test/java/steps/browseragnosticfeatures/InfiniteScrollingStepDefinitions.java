6f298af09d98628b31da17bd87fbc51ec6902688
noushinB <nou.babakhani@gmail.com>
2024-12-12 11:46:00 +0000

    StepDefinitions added to InfiniteScrollingStepDefinitions

--- src/test/java/steps/browseragnosticfeatures/InfiniteScrollingStepDefinitions.java
+++ src/test/java/steps/browseragnosticfeatures/InfiniteScrollingStepDefinitions.java
@@ -1,6 +1,9 @@
 package steps.browseragnosticfeatures;
 
 import core.pages.browseragnosticfeatures.InfiniteScrollingPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class InfiniteScrollingStepDefinitions {
     private final InfiniteScrollingPage infiniteScrollingPage;
@@ -8,4 +11,25 @@ public class InfiniteScrollingStepDefinitions {
     public InfiniteScrollingStepDefinitions() {
         infiniteScrollingPage = new  InfiniteScrollingPage();
     }
+
+    @And("the {string} section is visible on the page")
+    public void theSectionIsVisibleOnThePage(String arg0) {
+    }
+
+    @When("the user scrolls to the bottom of the visible content in the {string} section")
+    public void theUserScrollsToTheBottomOfTheVisibleContentInTheSection(String arg0) {
+    }
+
+    @Then("new content is dynamically loaded")
+    public void newContentIsDynamicallyLoaded() {
+    }
+
+    @And("the newly loaded content is appended below the existing content")
+    public void theNewlyLoadedContentIsAppendedBelowTheExistingContent() {
+    }
+
+    @And("there should be no full page reload during this process")
+    public void thereShouldBeNoFullPageReloadDuringThisProcess() {
+    }
+
 }
