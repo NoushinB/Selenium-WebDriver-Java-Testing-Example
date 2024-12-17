de0a36912030a17ea2fadefb8cf910a2bf402cf5
noushinB <nou.babakhani@gmail.com>
2024-12-17 16:21:54 +0000

    added element to InfiniteScrollingPage

--- src/test/java/core/pages/browseragnosticfeatures/InfiniteScrollingPage.java
+++ src/test/java/core/pages/browseragnosticfeatures/InfiniteScrollingPage.java
@@ -1,4 +1,12 @@
 package core.pages.browseragnosticfeatures;
 
-public class InfiniteScrollingPage {
+import core.pages.common.BasePage;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.WebElement;
+
+
+public class InfiniteScrollingPage extends BasePage {
+    @FindBy(id = "content")
+    private WebElement content;
+
 }
