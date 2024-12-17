134b94578b85a8c2513ad3360223d40597cf6f9f
noushinB <nou.babakhani@gmail.com>
2024-12-17 16:29:19 +0000

    added scrollToBottom function to InfiniteScrollingPage

--- src/test/java/core/pages/browseragnosticfeatures/InfiniteScrollingPage.java
+++ src/test/java/core/pages/browseragnosticfeatures/InfiniteScrollingPage.java
@@ -1,6 +1,7 @@
 package core.pages.browseragnosticfeatures;
 
 import core.pages.common.BasePage;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.WebElement;
 
@@ -8,5 +9,15 @@ import org.openqa.selenium.WebElement;
 public class InfiniteScrollingPage extends BasePage {
     @FindBy(id = "content")
     private WebElement content;
+
+    /**
+     * Scrolls down to the bottom of the page using JavaScript.
+     */
+    public void scrollToBottom() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+    }
+
+
 
 }
