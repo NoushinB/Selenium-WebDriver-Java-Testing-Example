c27ba2815350e10ff50308a2a55448ff1a4c7988
noushinB <nou.babakhani@gmail.com>
2025-01-06 14:12:18 +0000

    Added dialogBoxStepDefinitions.java

---
+++ src/test/java/steps/browseragnosticfeatures/dialogBoxStepDefinitions.java
@@ -0,0 +1,90 @@
+package steps.browseragnosticfeatures;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class dialogBoxStepDefinitions {
+    @When("the user click the {string} button")
+    public void the_user_click_the_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("an alert should appear with the text {string}")
+    public void an_alert_should_appear_with_the_text(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user accept the alert")
+    public void the_user_accept_the_alert() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user accept the confirmation dialog")
+    public void the_user_accept_the_confirmation_dialog() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the text {string} should appear in the confirmation message area")
+    public void the_text_should_appear_in_the_confirmation_message_area(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user cancel the confirmation dialog")
+    public void the_user_cancel_the_confirmation_dialog() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user type {string} into the prompt dialog")
+    public void the_user_type_into_the_prompt_dialog(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user confirm the prompt dialog")
+    public void the_user_confirm_the_prompt_dialog() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the text {string} should appear in the prompt message area")
+    public void the_text_should_appear_in_the_prompt_message_area(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user cancel the prompt dialog")
+    public void the_user_cancel_the_prompt_dialog() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the prompt message area should remain empty or display a default state")
+    public void the_prompt_message_area_should_remain_empty_or_display_a_default_state() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the modal dialog should appear")
+    public void the_modal_dialog_should_appear() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user click the {string} button in the modal")
+    public void the_user_click_the_button_in_the_modal(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the text {string} should appear in the modal message area")
+    public void the_text_should_appear_in_the_modal_message_area(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
