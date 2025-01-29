704f59ee1c1c8f31a85df6341ab682baf890f820
noushinB <nou.babakhani@gmail.com>
2025-01-29 15:23:58 +0000

    added LoginFormStepDefinitions

---
+++ src/test/java/steps/pageobjectmodel/LoginFormStepDefinitions.java
@@ -0,0 +1,28 @@
+package steps.pageobjectmodel;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginFormStepDefinitions {
+    @When("the user enters {string} in the username field")
+    public void the_user_enters_in_the_username_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user enters {string} in the password field")
+    public void the_user_enters_in_the_password_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user clicks the {string} button")
+    public void the_user_clicks_the_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user should see {string}")
+    public void the_user_should_see(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
