f2a07e9b1144e11442c79b93c756c94d65b8832b
noushinB <nou.babakhani@gmail.com>
2025-01-30 11:19:07 +0000

    added elements to LoginFormPage

--- src/test/java/core/pages/pageobjectmodel/LoginFormPage.java
+++ src/test/java/core/pages/pageobjectmodel/LoginFormPage.java
@@ -1,6 +1,24 @@
 package core.pages.pageobjectmodel;
 
 import core.pages.common.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginFormPage extends BasePage {
+
+    // Finding the username input field by its ID
+    @FindBy(id = "username")
+    private WebElement usernameInput;
+
+    // Finding the password input field by its ID
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    // Finding the submit button by its tag and class
+    @FindBy(xpath = "//button[@type='submit' and contains(@class, 'btn-outline-primary')]")
+    private WebElement submitButton;
+
+    // Finding the invalid credentials alert by its ID
+    @FindBy(id = "invalid")
+    private WebElement invalidCredentialsAlert;
 }
