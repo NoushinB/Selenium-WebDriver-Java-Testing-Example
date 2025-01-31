ffcae32299d10da02066cfbc1b0a7a8418387f58
noushinB <nou.babakhani@gmail.com>
2025-01-31 12:38:08 +0000

    LoginFormStepDefinitions constructor added

--- src/test/java/steps/pageobjectmodel/LoginFormStepDefinitions.java
+++ src/test/java/steps/pageobjectmodel/LoginFormStepDefinitions.java
@@ -1,13 +1,20 @@
 package steps.pageobjectmodel;
 
+import core.pages.pageobjectmodel.LoginFormPage;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginFormStepDefinitions {
+
+    final private LoginFormPage loginFormPage;
+
+    public LoginFormStepDefinitions() {
+        loginFormPage = new LoginFormPage();
+    }
+
     @When("the user enters {string} in the username field")
     public void the_user_enters_in_the_username_field(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("the user enters {string} in the password field")
     public void the_user_enters_in_the_password_field(String string) {
